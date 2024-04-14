56b535dfb69599f28274dab47a77af37dd209f8c
mojvaf <mojgan@zula-tribe.com>
2024-04-14 15:25:20 +0200

    create cabin

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://egcfiulpxehcdgbogbmd.supabase.co";
+export const supabaseUrl = "https://egcfiulpxehcdgbogbmd.supabase.co";
 // eslint-disable-next-line no-undef
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnY2ZpdWxweGVoY2RnYm9nYm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTE4MjUsImV4cCI6MjAyODQyNzgyNX0.63hAsN1Dbz9SwlOpWS0x7ty-OLw3kdVMGOuXMm2S0pk";

995186133b4fdf11489f33365cd3ca5e279ef362
mojvaf <mojgan@zula-tribe.com>
2024-06-11 13:09:08 +0200

    clean up

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -15,6 +15,7 @@ import { useCheckOut } from "../check-in-out/useCheckOut";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteBooking } from "./useDeleteBooking";
+import Empty from "../../ui/Empty";
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
@@ -29,6 +30,7 @@ function BookingDetail() {
   const { deleteBooking, isDeleting } = useDeleteBooking();
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   const { status, id: bookingId } = booking;
 

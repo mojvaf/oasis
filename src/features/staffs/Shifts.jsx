05c6c8351509b654c885173a705f93a5e85dfa0d
mojvaf <mojgan@zula-tribe.com>
2024-08-08 15:58:44 +0200

    draggable function

--- src/features/staffs/Shifts.jsx
+++ src/features/staffs/Shifts.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Shift from "./Shift";
 import styled from "styled-components";
 
-const Shifts = () => {
+const StyledDropArea = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: aquamarine;
+  opacity: 1;
+  transition: all 0.3s ease-in-out;
+`;
+
+const StyledArea = styled.div`
+  width: 100%;
+  height: 200px;
+  border: 2px dashed #ccc;
+`;
+const StyledPart = styled.div`
+  opacity: 0;
+`;
+const Shifts = ({ draggingItem, setDraggingItem }) => {
+  const [showDrop, setShowDrop] = useState(false);
+
+  const handleDrop = () => {
+    console.log(`Dropped item with id: ${draggingItem}`);
+    // Handle the drop logic here, such as updating state or making an API call
+    setShowDrop(false);
+    setDraggingItem(null);
+  };
+
   return (
-    <>
-      <Shift day="Monday" color="blue"></Shift>
-      <Shift day="Tuesday" color="blue"></Shift>
-      <Shift day="Wednesday" color="blue"></Shift>
-      <Shift day="Thursday" color="blue"></Shift>
-      <Shift day="Friday" color="blue"></Shift>
-      <Shift day="Saturday" color="blue"></Shift>
-      <Shift day="Sunday" color="blue"></Shift>
-    </>
+    <StyledArea
+      onDragEnter={() => setShowDrop(true)}
+      onDragOver={(e) => e.preventDefault()}
+      onDragLeave={() => setShowDrop(false)}
+      onDrop={handleDrop}
+    >
+      {showDrop ? <StyledDropArea /> : <StyledPart />}
+      <p>drop part</p>
+    </StyledArea>
   );
 };
 
--- src/features/staffs/Staff.jsx
+++ src/features/staffs/Staff.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import Spinner from "../../ui/Spinner";
 import { StyledStaffBox, StyledCircularImage } from "./StaffBox";
 import styled from "styled-components";
+import { useCreateStaff } from "./useCreateStaffs";
 
 const StyledBorder = styled.div`
   border: 1px solid var(--color-grey-600);
 `;
 
-const Staff = ({ staffs, isLoading }) => {
-  const [draggingItem, setDraggingItem] = useState(null);
+const Staff = ({ staffs, isLoading, setDraggingItem }) => {
+  const { isCreating, createStaff } = useCreateStaff();
+
+  function handleOnDrag(e) {}
 
   return (
     <StyledStaffBox>
       {staffs.map((st) => (
-        <StyledBorder key={st.id} draggable>
+        <StyledBorder
+          key={st.id}
+          draggable
+          onDragStart={() => setDraggingItem(st.id)}
+          onDragEnd={() => setDraggingItem(null)}
+        >
           <p>{st.name}</p>
           {isLoading ? <Spinner /> : <StyledCircularImage src={st.image} />}
         </StyledBorder>

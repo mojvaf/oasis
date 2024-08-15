1d9cf038bfe1fe2fb7b04fc3c174a3a72ce6f905
mojvaf <mojgan@zula-tribe.com>
2024-08-15 13:52:52 +0200

    create and update shifts

--- src/services/apiShifts.js
+++ src/services/apiShifts.js
@@ -1,1 +1,26 @@
+import supabase, { supabaseUrl } from "./supabase";
 
+export async function getShifts() {
+  const { data, error } = await supabase.from("shifts").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Shifts could not be loaded");
+  }
+
+  return data;
+}
+
+export async function createShift(newShift) {
+  const { data, error } = await supabase
+    .from("shifts")
+    .insert([newShift])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Shift could not be created");
+  }
+
+  return data;
+}

d597c6af6168ac324fb878724e590d0a9dc2bf91
mojvaf <mojgan@zula-tribe.com>
2024-09-16 23:06:48 +0200

    new button added

--- src/features/staffs/Staff.jsx
+++ src/features/staffs/Staff.jsx
@@ -3,6 +3,8 @@ import Spinner from "../../ui/Spinner";
 import { StyledStaffBox, StyledCircularImage } from "./StaffBox";
 import styled from "styled-components";
 import { useDeleteStaff } from "./useDeleteStaffs";
+import Button from "../../ui/Button";
+import Row from "../../ui/Row";
 
 const StyledBorder = styled.div`
   border: 1px solid var(--color-grey-600);
@@ -24,23 +26,26 @@ const Staff = ({ staffs, isLoading, setDraggingItem }) => {
   const { isDeleting, deleteStaff } = useDeleteStaff();
 
   return (
-    <StyledStaffBox>
-      {staffs.map((item) => (
-        <StyledBorder
-          key={item.id}
-          draggable
-          onDragStart={() => handleDragStart(item)}
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          <p>{item.name}</p>
-          <button onClick={() => deleteStaff(item.id)} disabled={isDeleting}>
-            Delete
-          </button>
-          {isLoading ? <Spinner /> : <StyledCircularImage src={item.image} />}
-        </StyledBorder>
-      ))}
-    </StyledStaffBox>
+    <Row type="verticalAndLeft">
+      <StyledStaffBox>
+        {staffs.map((item) => (
+          <StyledBorder
+            key={item.id}
+            draggable
+            onDragStart={() => handleDragStart(item)}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <p>{item.name}</p>
+            {isLoading ? <Spinner /> : <StyledCircularImage src={item.image} />}
+            <button onClick={() => deleteStaff(item.id)} disabled={isDeleting}>
+              Delete
+            </button>
+          </StyledBorder>
+        ))}
+      </StyledStaffBox>
+      <Button variation="primary">Add a new staff</Button>
+    </Row>
   );
 };
 
--- src/features/staffs/Staffs.jsx
+++ src/features/staffs/Staffs.jsx
@@ -5,26 +5,20 @@ import Shifts from "./Shifts";
 import Spinner from "../../ui/Spinner";
 import { useStaffs } from "./useStaffs";
 
-const StyledStaffsLayout = styled.div`
-  display: grid;
-  grid-template-rows: repeat(2, auto);
-  gap: 0.4rem;
-`;
-
 const Staffs = () => {
   const { isLoading, staffs } = useStaffs();
   const [draggingItem, setDraggingItem] = useState(null);
 
   if (isLoading) return <Spinner />;
   return (
-    <StyledStaffsLayout>
+    <div>
       <Staff
         staffs={staffs}
         isLoading={isLoading}
         setDraggingItem={setDraggingItem}
       />
       <Shifts draggingItem={draggingItem} setDraggingItem={setDraggingItem} />
-    </StyledStaffsLayout>
+    </div>
   );
 };
 

8db12e8e2b4eac7bb0f06513f1d467185682ea78
mojvaf <mojgan@zula-tribe.com>
2024-09-26 21:36:40 +0200

    added a edit button

--- src/features/staffs/Staff.jsx
+++ src/features/staffs/Staff.jsx
@@ -3,12 +3,14 @@ import Spinner from "../../ui/Spinner";
 import { StyledStaffBox, StyledCircularImage } from "./StaffBox";
 import styled from "styled-components";
 import { useDeleteStaff } from "./useDeleteStaffs";
-
+import CreateStaffForm from "./CreateStaffForm";
 const StyledBorder = styled.div`
   border: 1px solid var(--color-grey-600);
 `;
 
 const Staff = ({ staffs, isLoading, setDraggingItem }) => {
+  const [show, setShow] = useState(false);
+
   const handleDragStart = (item) => {
     setDraggingItem(item);
   };
@@ -24,23 +26,35 @@ const Staff = ({ staffs, isLoading, setDraggingItem }) => {
   const { isDeleting, deleteStaff } = useDeleteStaff();
 
   return (
-    <StyledStaffBox>
-      {staffs.map((item) => (
-        <StyledBorder
-          key={item.id}
-          draggable
-          onDragStart={() => handleDragStart(item)}
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          <p>{item.name}</p>
-          {isLoading ? <Spinner /> : <StyledCircularImage src={item.image} />}
-          <button onClick={() => deleteStaff(item.id)} disabled={isDeleting}>
-            Delete
-          </button>
-        </StyledBorder>
-      ))}
-    </StyledStaffBox>
+    <>
+      <StyledStaffBox>
+        {staffs.map((item) => (
+          <StyledBorder
+            key={item.id}
+            draggable
+            onDragStart={() => handleDragStart(item)}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <p>{item.name}</p>
+            {isLoading ? <Spinner /> : <StyledCircularImage src={item.image} />}
+            <div>
+              {" "}
+              <button onClick={() => setShow(!show)} disabled={isDeleting}>
+                Edit
+              </button>
+              <button
+                onClick={() => deleteStaff(item.id)}
+                disabled={isDeleting}
+              >
+                Delete
+              </button>
+            </div>
+          </StyledBorder>
+        ))}
+      </StyledStaffBox>
+      {show && <CreateStaffForm />}
+    </>
   );
 };
 
